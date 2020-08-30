906fff5cedc77253a85e13f63a980d70bb2905e3
Jeffrey Smith Bustamante <jeffreyrsb2@gmail.com>
2020-08-30 16:00:04 -0500

    redondear dolares

--- conversor.py
+++ conversor.py
@@ -2,5 +2,6 @@ pesos = input ("¿Cuántos soles tienes?: ")
 pesos = float(pesos)
 valor_dolar = 3.56
 dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
 dolares = str(dolares)
 print("Tienes $" + dolares + " dólares")
