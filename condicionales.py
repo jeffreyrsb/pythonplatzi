9128ccc93bad2a5e253a25eb31e3881344e70a79
Jeffrey Smith Bustamante <jeffreyrsb2@gmail.com>
2020-08-30 16:39:25 -0500

    codigo condicionales

---
+++ condicionales.py
@@ -0,0 +1,13 @@
+# edad = int(input("Escribe tu edad: "))
+# if edad > 17:
+#     print("Eres mayor de edad ;)")
+# else:
+#     print("Eres menor de edad")
+
+num = int(input("Escribe un número: "))
+if num>5:
+    print("Es mayor a 5")
+if num == 5:
+    print("Es igual a 5")
+if num < 5:
+    print("Es menor a 5")
