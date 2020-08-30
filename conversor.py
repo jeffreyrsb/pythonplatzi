fd9c09ae869857dc9ea4d2439eef2fe990ae2b24
Jeffrey Smith Bustamante <jeffreyrsb2@gmail.com>
2020-08-30 16:04:35 -0500

    corregir producto

--- conversor.py
+++ conversor.py
@@ -1,7 +1,7 @@
 soles = input ("¿Cuántos soles tienes?: ")
 soles = float(soles)
 valor_dolar = 3.56
-dolares = soles * valor_dolar
+dolares = soles / valor_dolar
 dolares = round(dolares, 2)
 dolares = str(dolares)
 print("Tienes $" + dolares + " dólares")
