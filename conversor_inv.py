1b467a12a9e72e8c98bb23fa8ac9a97ebdf9f06d
Jeffrey Smith Bustamante <jeffreyrsb2@gmail.com>
2020-08-30 16:12:24 -0500

    corrección de código

--- conversor_inv.py
+++ conversor_inv.py
@@ -2,6 +2,6 @@ dolares = input ("¿Cuántos dólares tienes?: ")
 dolares = float(dolares)
 valor_soles = 1/3.56
 soles = dolares / valor_soles
-soles = round(dolares, 2)
-soles = str(dolares)
-print("Tienes $" + dolares + " dólares")
+soles = round(soles, 2)
+soles = str(soles)
+print("Tienes S/." + soles + " soles")
