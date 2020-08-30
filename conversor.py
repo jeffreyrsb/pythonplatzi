f1259a84d01197663330aca3667957e18798feb0
Jeffrey Smith Bustamante <jeffreyrsb2@gmail.com>
2020-08-30 16:55:10 -0500

    conversor internacional

--- conversor.py
+++ conversor.py
@@ -1,7 +1,46 @@
-soles = input ("¿Cuántos soles tienes?: ")
-soles = float(soles)
-valor_dolar = 3.56
-dolares = soles / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
+menu="""
+Bienvenido al conversor de monedas 💲
+
+1. Soles
+2. Pesos colombianos
+3. Pesos argentinos
+4. Pesos mexicanos
+
+Elige una opción: """
+
+opcion=int(input(menu))
+
+if opcion==1:
+    soles = input("¿Cuántos soles tienes?: ")
+    soles = float(soles)
+    valor_dolar = 3.56
+    dolares = soles / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion==2:
+    pesos = input("¿Cuántos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 3:
+    pesos = input("¿Cuántos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 4:
+    pesos = input("¿Cuántos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+else:
+    print("Ingresa una opción correcta 🤡")
