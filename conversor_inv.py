0e398937fa385c2a814cbab7aa22ef2da24e04cb
Jeffrey Smith Bustamante <jeffreyrsb2@gmail.com>
2020-08-30 16:10:22 -0500

    creacion de codigo a la inversa

---
+++ conversor_inv.py
@@ -0,0 +1,7 @@
+dolares = input ("¿Cuántos dólares tienes?: ")
+dolares = float(dolares)
+valor_soles = 1/3.56
+soles = dolares / valor_soles
+soles = round(dolares, 2)
+soles = str(dolares)
+print("Tienes $" + dolares + " dólares")
