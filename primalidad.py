1ded1e5440d0460c3aec82b16a646e1c03faa2bb
Jeffrey Smith Bustamante <jeffreyrsb2@gmail.com>
2020-09-02 08:47:06 -0500

    primalidad wilson

--- primalidad.py
+++ primalidad.py
@@ -1,24 +1,42 @@
-def es_primo(numero):
-    contador = 0
+# def es_primo(numero):
+#     contador = 0
 
-    for i in range (1, numero + 1):
-        if i == 1 or i == numero:
-            continue
-        if numero % i == 0:
-            contador += 1
-    if contador == 0 and numero != 1:
-        return True
-    else:
-        return False
+#     for i in range (1, numero + 1):
+#         if i == 1 or i == numero:
+#             continue
+#         if numero % i == 0:
+#             contador += 1
+#     if contador == 0 and numero != 1:
+#         return True
+#     else:
+#         return False
+
+
+# def run():
+    # numero = int(input("Escribe un número: "))
+    # if es_primo(numero):
+    #     print("Es tu prima!")
+    # else:
+    #     print("No es tu prima.")
+
+
+def factorial(n):
+    fact = 1
+    
+    for i in range (1, n + 1):
+        fact *= i
+    
+    return fact
 
 
 def run():
-    numero = int(input("Escribe un número: "))
-    if es_primo(numero):
-        print("Es tu prima!")
+    numero = int(input("Escoge un numero: "))
+    wilson = factorial(numero - 1) + 1
+    if wilson % numero == 0:
+        print("Es primo.")
     else:
-        print("No es tu prima.")
-
+        print("No es primo.")
+    
 
 if __name__ == "__main__":
     run()
